460221b048a4fcf663f6b16e3087053c5d4d826c
Ryan Du Bois <github@ryandubois.net>
2009-04-15 18:33:57 -0700

    Only do git_setup during installation: fixes annoying shell errors when starting multiple terminals at once.

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@ cp -i bash_profile ~/.bash_profile
 cp -i vimrc ~/.vimrc
 cp -i gvimrc ~/.gvimrc
 cp -i bashrc ~/.bashrc
+
+source ~/.bashrc
+
+git_setup
+
+echo "Please run the following commands to finish the installation:"
+echo ". ~/.bashrc"
